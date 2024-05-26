29187cf3d1e67ea6f704c8f0e5af692eaba4b87a
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-05-26 12:25:41 -0700

    Revert "enchilada: Ship cgroups.json and task_profiles.json"
    This reverts commit b47cacd8670da44038903acc5f322e3a4b04ebe5.

--- device.mk
+++ device.mk
@@ -53,11 +53,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.oneplus_sdm845
 
-# Power
-PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
